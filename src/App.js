c65de70a24ae750aaf164a35603a51fbc974795d
William Liu <110699040+waileungl@users.noreply.github.com>
2023-01-15 01:55:49 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/fV-EMqC0bztQiZ6g/scene.splinecode" />
+  );
+}
